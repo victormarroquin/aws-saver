a425de1a36d69c5d951ab5128210e14b36c61a01
Victor Marroquin <victorr.marroquin@gmail.com>
2017-04-14 09:45:21 -0600

    Adds multi tag functionality

--- saver.sh
+++ saver.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
-ids=$(/usr/local/bin/aws --region us-east-1 ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[?Key==`'$1'`].Value[]]' | grep -B 2 $2 | grep "\"i-" | cut -d "\"" -f 2 | sed -e 's/,//g')
-if [ "$3" == "list" ]; then
+key="0"
+for i in "${@:2}"; do
+        if [ "$key" -eq 0 ]; then
+                texto="$texto Name=tag:$i"; key=1;
+        else
+                texto="$texto,Values=$i"; key=0
+        fi
+done
+echo $texto
+ids=$(/usr/local/bin/aws --region us-east-1 ec2 describe-instances --query 'Reservations[*].Instances[*].[InstanceId,Tags[].Value[]]' --filters $texto | jq -r ".[][][0]")
+if [ "$1" == "list" ]; then
         echo -e "Collected Ids:\n\n${ids}"
 echo -e "\nDone!"
 
-elif [ "$3" == "status" ]; then
-    echo "Collecting $2 Instance IDs"
-        /usr/local/bin/aws --region us-east-1 ec2 describe-instances --query 'Reservations[*].Instances[*].[Tags[?Key==`'$1'`].Value[],State.Name,InstanceId,Tags[?Key==`Name`].Value[]]' --output text | grep -A 2 $2 | sed -e 's/'$2'//g'
-echo -e "\nDone!"
-
-elif [ "$3" == "stop" ]; then
+elif [ "$1" = "stop" ]; then
         /usr/local/bin/aws --region us-east-1 ec2 --instance-ids stop-instances ${ids}
 echo -e "\nDone!"
 
-elif [ "$3" == "start" ]; then
+elif [ "$1" = "start" ]; then
         /usr/local/bin/aws --region us-east-1 ec2 --instance-ids start-instances ${ids}
 echo -e "\nDone!"
 
 else
-        echo -e "Run the command like the following example: \e[1;31msaver Enable start\e[0m"
+        echo -e "Run the command like the following example: \e[1;31NNameTag ValueTag Action (start, stop, list)\e[0m"
 fi
